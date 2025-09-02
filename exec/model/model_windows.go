6de9d95b6b92a48e157a23e1f5467d4cbdd367f1
穹谷 <changjun.xcj@alibaba-inc.com>
2025-09-02 12:02:59 +0800

    Chore: Support cross-platform compilation and verification
    Signed-off-by: 穹谷 <changjun.xcj@alibaba-inc.com>

--- exec/model/model_windows.go
+++ exec/model/model_windows.go
@@ -1,5 +1,5 @@
-//go:build darwin
-// +build darwin
+//go:build windows
+// +build windows
 
 /*
  * Copyright 1999-2020 Alibaba Group Holding Ltd.
@@ -13,7 +13,7 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * See the License for the specific license governing permissions and
  * limitations under the License.
  */
 
